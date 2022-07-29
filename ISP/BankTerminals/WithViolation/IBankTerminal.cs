2c2e2418cb36d71e9b6dd9d7b55ca6ef441d619e
jeftegoes <jefte.goes.dev@outlook.com>
2022-07-28 23:28:23 -0300

    Adding example to describe ISP violation.

---
+++ ISP/BankTerminals/WithViolation/IBankTerminal.cs
@@ -0,0 +1,17 @@
+namespace WithViolation
+{
+    public interface IBankTerminal
+    {
+        void Start();
+        void Stop();
+        void Ping();
+        void BankHostTest();
+        void Purchase(decimal amount, string checkId);
+        void CancelPayment(string checkId, decimal amount);
+        void InterruptTransaction();
+
+        event EventHandler<PaymentOperationCompletedEventArgs> PaymentCompleted;
+        event EventHandler<PaymentOperationCompletedEventArgs> CancellationCompleted;
+        event EventHandler<TransactionCompletedEventArgs> TransactionCompleted;
+    }
+}
---
+++ ISP/BankTerminals/WithViolation/ZapTerminal.cs
@@ -0,0 +1,44 @@
+namespace WithViolation
+{
+    public class ZapTerminal : IBankTerminal
+    {
+        public event EventHandler<PaymentOperationCompletedEventArgs> PaymentCompleted;
+        public event EventHandler<PaymentOperationCompletedEventArgs> CancellationCompleted;
+        public event EventHandler<TransactionCompletedEventArgs> TransactionCompleted;
+
+        public void BankHostTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelPayment(string checkId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InterruptTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Ping()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Purchase(decimal amount, string checkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
